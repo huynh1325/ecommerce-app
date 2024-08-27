380fad2ddf661e3a063a45ca4c13b01a3e2eb5ba
huynh <huynhnguyen120202@gmail.com>
2024-08-28 00:05:43 +0700

    Base Project

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+import Home from '~/pages/Home';
+import Account from '~/pages/Account';
+
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/account', component: Account },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
