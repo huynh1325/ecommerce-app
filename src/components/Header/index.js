84c09cac9896a332e39ea784df192609c2707b4b
huynh <huynhnguyen120202@gmail.com>
2024-10-08 22:28:16 +0700

    Login UI

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,48 +9,63 @@ import {
     faCartShopping,
     faLocationDot,
 } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+import Login from '../Login';
 
 const cx = classNames.bind(styles);
 
 const Header = () => {
+    const [modalLogin, setModalLogin] = useState(false);
+
+    const openModal = () => {
+        setModalLogin(true);
+    };
+
+    const closeModal = () => {
+        setModalLogin(false);
+    };
+
     return (
-        <div className={cx('header')}>
-            <div className={cx('logo')}>
-                <img className={cx('logo-img')} src={toko} alt="logo" />
-                <span className={cx('logo-text')}>Tốt & Nhanh</span>
-            </div>
-            <div className={cx('header-btn')}>
-                <div className={cx('search')}>
-                    <div className={cx('search-home')}>
-                        <FontAwesomeIcon icon={faMagnifyingGlass} className={cx('search-logo')} />
-                        <input type="text" placeholder={'Tìm kiếm...'} className={cx('search-input')} />
-                        <button className={cx('search-btn')}>Tìm kiếm</button>
+        <>
+            <div className={cx('header')}>
+                <div className={cx('logo')}>
+                    <img className={cx('logo-img')} src={toko} alt="logo" />
+                    <span className={cx('logo-text')}>Tốt & Nhanh</span>
+                </div>
+                <div className={cx('header-btn')}>
+                    <div className={cx('search')}>
+                        <div className={cx('search-home')}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} className={cx('search-logo')} />
+                            <input type="text" placeholder={'Tìm kiếm...'} className={cx('search-input')} />
+                            <button className={cx('search-btn')}>Tìm kiếm</button>
+                        </div>
+                        <div className={cx('search-account')}>
+                            <button className={cx('search-account-home')}>
+                                <FontAwesomeIcon icon={faHouse} className={cx('home-icon')} />
+                                <span className={cx('icon-text')}>Trang chủ</span>
+                            </button>
+                            <button onClick={openModal} className={cx('search-account-btn')}>
+                                <FontAwesomeIcon icon={faFaceSmile} className={cx('react-icon')} />
+                                <span className={cx('icon-text')}>Tài khoản</span>
+                            </button>
+                            <div className={cx('cart-wrapper')}></div>
+                            <span className={cx('cart-icon')}>
+                                <FontAwesomeIcon icon={faCartShopping} />
+                                <span className={cx('cart-quantity')}>0</span>
+                            </span>
+                        </div>
                     </div>
-                    <div className={cx('search-account')}>
-                        <button className={cx('search-account-home')}>
-                            <FontAwesomeIcon icon={faHouse} className={cx('home-icon')} />
-                            <span className={cx('icon-text')}>Trang chủ</span>
-                        </button>
-                        <button className={cx('search-account-btn')}>
-                            <FontAwesomeIcon icon={faFaceSmile} className={cx('react-icon')} />
-                            <span className={cx('icon-text')}>Tài khoản</span>
-                        </button>
-                        <div className={cx('cart-wrapper')}></div>
-                        <span className={cx('cart-icon')}>
-                            <FontAwesomeIcon icon={faCartShopping} />
-                            <span className={cx('cart-quantity')}>0</span>
-                        </span>
+                    <div className={cx('location')}>
+                        <FontAwesomeIcon icon={faLocationDot} className={cx('location-icon')} />
+                        <span className={cx('location-text')}>Giao đến:</span>
+                        <a href="/" className={cx('location-href')}>
+                            Bạn muốn giao đến đâu?
+                        </a>
                     </div>
                 </div>
-                <div className={cx('location')}>
-                    <FontAwesomeIcon icon={faLocationDot} className={cx('location-icon')} />
-                    <span className={cx('location-text')}>Giao đến:</span>
-                    <a href="/" className={cx('location-href')}>
-                        Bạn muốn giao đến đâu?
-                    </a>
-                </div>
             </div>
-        </div>
+            <Login modalLogin={modalLogin} onClose={closeModal} />
+        </>
     );
 };
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,13 @@
 import Header from '~/components/Header';
-import Login from '~/components/Login';
 import './Home.module.scss';
 
-function Home() {
+const Home = () => {
     return (
         <>
             <Header />
-            <Login />
             <h2>Home page</h2>
         </>
     );
-}
+};
 
 export default Home;
